37c3696d690cdb220d35c3c2e89050b8a754216b
ryanjgross <ryanjgross@gmail.com>
2017-07-15 21:03:59 -0400

    Adding comments stuff

---
+++ src/app/home-feed/image-detail-tile/comment/comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
